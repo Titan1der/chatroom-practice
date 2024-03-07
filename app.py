42fb8955c933792830b0d53fe95d4d29c14676ab
Titan1Dev <106718090+titan1dev@users.noreply.github.com>
2024-03-07 16:29:13 +0900

    + Heroku intergration

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import websockets
 import json
 import asyncio
 import secrets
+import os
+import signal
 
 from websockets import WebSocketServerProtocol
 from chatroom import Chatroom
@@ -111,9 +113,15 @@ async def handler(websocket : WebSocketServerProtocol):
             
 
 async def main():
-    async with websockets.serve(handler, "localhost", 8001):
-        print("Now listening on port8001:")
-        await asyncio.Future() # run infinitely
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    
+    port = int(os.environ.get("PORT", "8001"))
+    
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
