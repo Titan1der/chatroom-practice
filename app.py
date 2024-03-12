fa9b83fb42c5769dbff26feac60f985fd3a4489c
Titan1Der <106718090+Titan1der@users.noreply.github.com>
2024-03-13 00:18:23 +0900

    Revising the server-side code .rev1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 import signal
 
 from controller.functions_py.constants import *
-
+from models.app_server import AppServer
 import logging
 # Configure logging
 logging.basicConfig(
@@ -12,34 +12,25 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
     )
 
-from controller.functions_py.room_session_handler import handler
-            
 
-def get_working_environment():
-    if "chatroom-practice" in os.getcwd():
-        return ENV_DEV
-    else:
-        return ENV_PROD
-    
-            
 async def main():
-    # Current environment
-    environment = get_working_environment()
-    logging.info(f"Current environment: {environment}")
-    
+    app = AppServer()
+
     # Set the stop condition when receiving SIGTERM.
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
 
-    if environment == ENV_PROD:
+    if app.running_environment == ENV_PROD:
         loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
     
     port = int(os.environ.get("PORT", "8001"))
     
-    async with websockets.serve(handler, "", port):
+    async with websockets.serve(app.handle_requests, "", port):
         logging.info("Listening on websocket port:8001")
         await stop
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+    
+    
